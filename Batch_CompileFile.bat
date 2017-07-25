f81f32505090e62aaf2dfcbcbffc99e78c1d3a7c
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2017-07-24 23:57:29 -0400

    ADD: propuesta and actual thesis writing

---
+++ Batch_CompileFile.bat
@@ -0,0 +1,24 @@
+rem --- 'rem' or '::' is the key word for comments in batch files like this
+
+rem --- Call cleanup script in order to delete all auxiliary files ---
+rem --- Sometimes after error messages there are still errors in the auxiliary files even if the root of the error was corrected in the tex file
+:: ---
+call my_Batch_CleanUp.bat
+:: ---
+
+rem --- First LaTeX run (pdflatex) with special options (more memory) for large LaTeX projects and tikz stuff (-shell-escape) ---
+rem --- I use MiKTeX -- maybe you need another syntax if you use TeX Live for example ---
+:: ---
+pdflatex --extra-mem-top=60000000 -synctex=1 -shell-escape -interaction=batchmode MainFile.tex
+
+rem --- Execude biber if used ---
+:: ---
+biber	  MainFile
+:: ---
+
+rem --- Compile three times to make sure everything is correct ---
+:: ---
+pdflatex --extra-mem-top=60000000 -synctex=1 -shell-escape -interaction=batchmode MainFile.tex
+pdflatex --extra-mem-top=60000000 -synctex=1 -shell-escape -interaction=batchmode MainFile.tex
+pdflatex --extra-mem-top=60000000 -synctex=1 -shell-escape -interaction=batchmode MainFile.tex
+:: ---
---
+++ propuesta/Batch_CleanUp.bat
@@ -0,0 +1,15 @@
+:: Delete all auxiliary files
+
+del *.aux
+del *.log
+del *.gz
+del *.blg
+del *.bbl
+del *.lof
+del *.lot
+del *.out
+del *.ptc
+del *.toc
+del *blx.bib
+del *run.xml
+del *.bcf                                                                                                                                                  
